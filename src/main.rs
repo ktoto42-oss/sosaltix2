a35cf6003769a0e7b3e6f353c3f4b282d4a0d997
ktoto42-oss <d1533848@gmail.com>
2026-04-16 00:05:17 +0700

    penis

--- src/main.rs
+++ src/main.rs
@@ -11,27 +11,26 @@ use bootloader::{BootInfo, entry_point};
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use sosaltix2::memory::active_level_4_table;
-    use x86_64::VirtAddr;
-    use sosaltix2::memory::translate_addr;
+    use sosaltix2::memory;
+    use x86_64::{structures::paging::Page, VirtAddr};
+    use sosaltix2::memory::BootInfoFrameAllocator;
 
     println!("Welcome to Sosaltix2");
     sosaltix2::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    //let mut frame_allocator = memory::EmptyFrameAllocator;
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    //let page = Page::containing_address(VirtAddr::new(0));
 
-    let addresses = [
-        0xb8000,
-        0x201008,
-        0x0100_0020_1a10,
-        boot_info.physical_memory_offset,
-    ];
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
 
     #[cfg(test)]
@@ -54,3 +53,4 @@ fn panic(info: &PanicInfo) -> ! {
 fn panic(info: &PanicInfo) -> ! {
     sosaltix2::test_panic_handler(info)
 }
+
