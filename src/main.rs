9c36b5f0a0091b6ec136014b1eb62a19bcbf01a4
ktoto42-oss <d1533848@gmail.com>
2026-04-23 23:25:51 +0700

    siska

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,25 @@
-#![no_std]
-#![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(sosaltix2::test_runner)]
-#![reexport_test_harness_main = "test_main"]
+use std::env;
+use std::process::Command;
 
-use core::panic::PanicInfo;
-use sosaltix2::println;
-use bootloader::{BootInfo, entry_point};
-extern crate alloc;
-use sosaltix2::task::Task;
-use sosaltix2::task::executor::Executor;
-use sosaltix2::shell;
-
-entry_point!(kernel_main);
-
-fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use sosaltix2::memory;
-    use sosaltix2::allocator;
-    use x86_64::{structures::paging::Page, VirtAddr};
-    use sosaltix2::memory::BootInfoFrameAllocator;
-
-    println!("Welcome to Sosaltix2");
-
-    sosaltix2::init();
-
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe {
-        BootInfoFrameAllocator::init(&boot_info.memory_map)
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mode = args.get(1).map(|s| s.as_str()).unwrap_or("bios");
+    
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let img = match mode {
+        "bios" => format!("{}/bios.img", out_dir),
+        "uefi" => format!("{}/uefi.img", out_dir),
+        _ => panic!("unknown mode: {}", mode),
     };
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
-
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
-
-    allocator::init_heap(&mut mapper, &mut frame_allocator)
-        .expect("heap initialization failed");
-
-    // запуск задач
-    let mut executor = Executor::new();
-    executor.spawn(Task::new(shell::run_shell()));
-    executor.run();
-
-    #[cfg(test)]
-    test_main();
-
-    sosaltix2::hlt_loop();
+    
+    let mut cmd = Command::new("qemu-system-x86_64");
+    cmd.arg("-drive").arg(format!("format=raw,file={}", img));
+    
+    // Для отладки
+    if mode == "uefi" {
+        cmd.arg("-bios").arg("OVMF.4m.fd"); // путь к вашему OVMF
+    }
+    
+    let status = cmd.status().expect("failed to run QEMU");
+    std::process::exit(status.code().unwrap_or(1));
 }
-
-// тэстики
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    sosaltix2::hlt_loop();
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    sosaltix2::test_panic_handler(info)
-}
-
